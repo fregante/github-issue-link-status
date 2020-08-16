9809274f6c0e64355ca06a4684e6b2f5f3995d39
Federico Brigante <opensource@bfred.it>
2020-08-16 14:30:46 +0100

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -10,18 +10,21 @@ module.exports = {
 		options: './source/options'
 	},
 	plugins: [
-		new CopyWebpackPlugin([
-			{
-				from: '**',
-				context: 'source',
-				ignore: [
-					'*.js'
-				]
-			},
-			{
-				from: 'node_modules/webext-base-css/webext-base.css'
-			}
-		])
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: 'source',
+					globOptions: {
+						ignore: [
+							'**/*.js'
+						]
+					}
+				},
+				{
+					from: 'node_modules/webext-base-css/webext-base.css'
+				}
+			]
+		})
 	],
 	output: {
 		path: path.join(__dirname, 'distribution'),
