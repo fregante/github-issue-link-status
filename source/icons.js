98c5a7957f17c590134b120ac13df727f9b4ce27
Ritik Bheda <ritik.bheda@gmail.com>
2021-10-28 14:59:53 -0400

    cleaning comments

--- source/icons.js
+++ source/icons.js
@@ -3,14 +3,13 @@ import octicons from '@primer/octicons';
 export const openissue = octicons['issue-opened'].toSVG();
 
 export const closedissue = octicons['issue-closed'].toSVG();
-//issue-closed
+
 export const draftissue = octicons['issue-draft'].toSVG();
-//issue-draft
+
 export const openpullrequest = octicons['git-pull-request'].toSVG();
-//git-pull-request
+
 export const mergedpullrequest = octicons['git-merge'].toSVG();
-//git-merge
+
 export const closedpullrequest = octicons['git-pull-request-closed'].toSVG();
-//git-pull-request-closed
+
 export const draftpullrequest = octicons['git-pull-request-draft'].toSVG();
-//git-pull-request-draft
